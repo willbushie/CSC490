81193725eedfc61eb1389189c2775fe0e7ebb977
William Bushie <willbushie@gmail.com>
2022-03-27 16:09:56 -0500

    Huffman Code - compression algorithm HW complete

--- main.py
+++ main.py
@@ -1,82 +1,56 @@
-# CSC490 - String Searching using Hash Table implementation
+# CSC490 - Huffman Code Compression Algorithm
 
-"""
-Objectives:
-X    1. Select a hash function (modulo or multiplication).
-X    2. Select a collision function (speration chaining or open addressing).
-X    3. Use the provided poem (read it into the program).
-X    4. Insert the poem into the hash table.
-X    5. Use the hash table for string searching.
-"""
-
-# modulo for hashing and chaining for collision
-class HashTable: 
-    M = 100
-    def __init__(self) -> None:
-        self.array = [[],] * self.M # we have 31 buckets here
-
-    def Hash(self,key):
-        return key % self.M
-
-    def insert(self,key,value):
-        hashcode = self.Hash(key)
-        dictionary = {"key":hashcode,"index":[],"value":value}
-        found = len(self.array[hashcode]) > 0
-        if found is True:
-            for count in range(len(self.array[hashcode])): # looking at each item in the chain
-                if self.array[hashcode][count]["key"] == key:
-                    self.array[hashcode][count]["index"].append(value)#["index"][0])
-                    return hashcode
-            # add the new value on to the chain
-            self.array[hashcode].append(dictionary)
-            return hashcode
-        else:
-            self.array[hashcode].append(dictionary)
+class Node:
+    def __init__(self,symbol,value,left=None,right=None) -> None:
+        self.symbol = symbol
+        self.value = value
+        self.left = left
+        self.right = right
+        self.huff = ""
     
-    def search(self,key):
-        hashcode = self.Hash(key)
-        if len(self.array[hashcode]) > 0:
-            for count in range(len(self.array[hashcode])):
-                if self.array[hashcode][count]["key"] == key:
-                    return self.array[hashcode][count]["key"], self.array[hashcode][count]["index"], self.array[hashcode][count]["value"]
-            return None
+    def IsLeaf(self):
+        return self.left is None and self.right is None
+    
+    def UpdateHuff(self):
+        if not self.IsLeaf():
+            self.left.huff = self.huff + "0"
+            self.left.UpdateHuff()
+            self.right.huff = self.huff + "1"
+            self.right.UpdateHuff()
+    
+    def PrintHuff(self):
+        if not self.IsLeaf():
+            self.left.PrintHuff()
+            self.right.PrintHuff()
         else:
-            return None
+            print(f'{self.symbol} -- {self.value} --> {self.huff}')
 
-    def searchString(self,string):
-        for count in range(len(self.array)):
-            for innerCount in range(len(self.array[count])):
-                if self.array[count][innerCount]["index"] == string:
-                    return True
-                elif self.array[count][innerCount]["value"] == string:
-                    return True
-        return False
+class HuffmanTree:
+    def __init__(self) -> None:
+        self.root = None
 
-# read in a file and return a list
-def readFile(filename):
-    with open(filename) as file:
-        contents = file.readlines()
-    for line in range(len(contents)-1):
-        contents[line] = contents[line][:-1]
-    return contents
+    def BuildTree(self,input): # input is a dictionary of symbols as a key and frequency as the value
+        leafArray = []
+        for key in input:
+            leafNode = Node(key,input[key])
+            leafArray.append(leafNode)
+        while len(leafArray) > 1:
+            leafArray.sort(key=lambda x: x.value, reverse=True)
+            right = leafArray.pop()
+            left = leafArray.pop()
+            parentNode = Node(left.symbol + right.symbol, left.value + right.value, left, right)
+            leafArray.append(parentNode)
+        self.root = leafArray[0]
 
+    def UpdateHuff(self):
+        self.root.UpdateHuff()
+    
+    def PrintTable(self):
+        self.root.PrintHuff()
 
-# calling each hash methods - testing purposes
-def test1():
-    testlist = ["yes","no","maybe","so","this","is","a","testtt"]
-    table1 = HashTable()
-    for index in range(len(testlist)):
-        table1.insert(index,testlist[index])
-    for count in range(len(testlist)):
-        print(table1.search(count))
-
-def test2():
-    list = readFile("poem.txt")
-    table = HashTable()
-    for index in range(len(list)):
-        table.insert(index,list[index]) # insert the items from the poem into the hash table
-    for count in range(len(table.array)):
-        print(table.search(count)) # print out each item from the hash table
-    print(table.searchString("and")) # print out true, if that word is contained in the table
-
-test2()
+# create, and display huffman tree
+list = {"A":.02,"B":.03,"C":.04,"D":.06,"E":.07,"F":.08,"G":.09,"H":.11,"I":.2,"J":.2} #{"A":0.25,"B":0.21,"C":0.18,"D":0.14,"E":0.09,"F":0.07,"G":0.06}
+tree = HuffmanTree()
+tree.BuildTree(list)
+tree.UpdateHuff()
+tree.PrintTable()
